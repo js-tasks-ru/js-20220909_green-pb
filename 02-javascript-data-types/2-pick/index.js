4ec1af7b2a987729254825d76eacb63e69d40f92
Alex Kandaurov <alex@flexify.io>
2022-09-14 17:04:43 +0300

    tasks 2 and 3

--- 02-javascript-data-types/2-pick/index.js
+++ 02-javascript-data-types/2-pick/index.js
@@ -4,6 +4,20 @@
  * @param {...string} fields - the properties paths to pick
  * @returns {object} - returns the new object
  */
-export const pick = (obj, ...fields) => {
+export
+ const pick = (obj, ...fields) => {
+    let result = {};
+    for (let key of fields){
+        result[key] = obj[key];
+    }
 
+    return result;
 };
+
+// const fruits = {
+//     apple: 2,
+//     orange: 4,
+//     banana: 3
+//    };
+   
+//    console.log(pick(fruits, 'apple', 'banana')); // { apple: 2, banana: 3 }
