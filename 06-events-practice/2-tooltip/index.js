6269421ab8c8b074789111ef6913d911f30c4c37
Alex Kandaurov <alex@flexify.io>
2022-09-30 18:29:11 +0300

    task 06-2 pretty

--- 06-events-practice/2-tooltip/index.js
+++ 06-events-practice/2-tooltip/index.js
@@ -3,47 +3,37 @@ class Tooltip {
 
   static instance;
 
-  constructor(){
+  constructor() {
     if (Tooltip.instance) return Tooltip.instance;
     Tooltip.instance = this;
   }
 
   initialize() {
-    const elementsWithTooltips = document.querySelectorAll('[data-tooltip]');
-    elementsWithTooltips.forEach((element) => {
-      element.addEventListener('mousemove', (event) => this.move(event));
-      element.addEventListener('pointerover', (event) => this.over(event));
-      element.addEventListener('pointerout', (event) => this.out(event));
-    })
-
-    // console.log('init', elementsWithTooltips);
+    document.querySelectorAll('[data-tooltip]')
+      .forEach((element) => {
+        element.addEventListener('mousemove', (event) => this.move(event));
+        element.addEventListener('pointerover', (event) => this.over(event));
+        element.addEventListener('pointerout', (event) => this.out(event));
+      })
   }
 
   move(event) {
-    // console.log('move', event);
-
     this.element.style.position = "absolute";
     this.element.style.left = event.clientX + this.offset[0] + 'px';
     this.element.style.top = event.clientY + this.offset[1] + 'px';
-
   }
-  over(event) {
-    // console.log('over', event.target.dataset.tooltip);
 
+  over(event) {
     if (event.tooltipIsShown) return; // если уже показали, иначе при всплытии отработает еще раз
     this.render(event.target.dataset.tooltip);
     event.tooltipIsShown = true; // сработаем только на таргете, при всплытии уже не будем работать
   }
-  out(event) {
-    // console.log('out', event.target.dataset.tooltip);
 
+  out(event) {
     this.remove();
   }
 
-
   render(text) {
-    // console.log('render', text);
-
     this.element = document.createElement("div"); // (*)
     this.element.setAttribute("class", "tooltip");
     this.element.textContent = text;
