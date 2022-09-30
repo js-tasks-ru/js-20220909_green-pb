fa076502b939fa672b7e57b54cfa54ba312de5fa
Alex Kandaurov <alex@flexify.io>
2022-09-30 18:27:30 +0300

    task 06-2

--- 06-events-practice/2-tooltip/index.js
+++ 06-events-practice/2-tooltip/index.js
@@ -1,6 +1,61 @@
 class Tooltip {
-  initialize () {
+  offset = [10, 10];
 
+  static instance;
+
+  constructor(){
+    if (Tooltip.instance) return Tooltip.instance;
+    Tooltip.instance = this;
+  }
+
+  initialize() {
+    const elementsWithTooltips = document.querySelectorAll('[data-tooltip]');
+    elementsWithTooltips.forEach((element) => {
+      element.addEventListener('mousemove', (event) => this.move(event));
+      element.addEventListener('pointerover', (event) => this.over(event));
+      element.addEventListener('pointerout', (event) => this.out(event));
+    })
+
+    // console.log('init', elementsWithTooltips);
+  }
+
+  move(event) {
+    // console.log('move', event);
+
+    this.element.style.position = "absolute";
+    this.element.style.left = event.clientX + this.offset[0] + 'px';
+    this.element.style.top = event.clientY + this.offset[1] + 'px';
+
+  }
+  over(event) {
+    // console.log('over', event.target.dataset.tooltip);
+
+    if (event.tooltipIsShown) return; // если уже показали, иначе при всплытии отработает еще раз
+    this.render(event.target.dataset.tooltip);
+    event.tooltipIsShown = true; // сработаем только на таргете, при всплытии уже не будем работать
+  }
+  out(event) {
+    // console.log('out', event.target.dataset.tooltip);
+
+    this.remove();
+  }
+
+
+  render(text) {
+    // console.log('render', text);
+
+    this.element = document.createElement("div"); // (*)
+    this.element.setAttribute("class", "tooltip");
+    this.element.textContent = text;
+    document.body.append(this.element);
+  }
+
+  remove() {
+    this.element.remove();
+  }
+
+  destroy() {
+    this.remove();
   }
 }
 
