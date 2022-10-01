5d1f03fb1248836432c6f102778110ae7bd42a72
Alex Kandaurov <alex@flexify.io>
2022-10-01 23:39:35 +0300

    06-2 document fix

--- 06-events-practice/2-tooltip/index.js
+++ 06-events-practice/2-tooltip/index.js
@@ -10,33 +10,29 @@ class Tooltip {
   }
 
   initialize() {
-    document.addEventListener('pointerover', (event) => this.over(event), this.controller.signal);
+    document.addEventListener('pointerover', (event) => this.onover(event), this.controller.signal);
   }
 
-  over(event) {
-    let targetElement = event.target;
-    while (targetElement) { // на случай вложенных тегов ищем родителя с dataset.tooltip
-      if (targetElement.dataset.tooltip) break;
-      targetElement = targetElement.parentElement;
-    }
+  onover(event) {
+    const targetElement = event.target.closest('[data-tooltip]');
     if (!targetElement) return;
-    document.addEventListener('pointermove', (event) => this.move(event), this.controller.signal);
-    document.addEventListener('pointerout', (event) => this.out(event), this.controller.signal);
+    document.addEventListener('pointermove', (event) => this.onmove(event), this.controller.signal);
+    document.addEventListener('pointerout', (event) => this.onout(event), this.controller.signal);
     this.render(targetElement.dataset.tooltip);
   }
 
-  move(event) {
+  onmove(event) {
     this.element.style.position = "absolute";
     this.element.style.left = event.clientX + this.offset[0] + 'px';
     this.element.style.top = event.clientY + this.offset[1] + 'px';
   }
 
-  out(event) {
+  onout(event) {
     this.remove();
   }
 
   render(text) {
-    this.element = document.createElement("div"); // (*)
+    this.element = document.createElement("div");
     this.element.setAttribute("class", "tooltip");
     this.element.textContent = text;
     document.body.append(this.element);
